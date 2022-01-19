a5010c60bf578c1a3e82dae5ac8991bc4a752ba3
Rimac48 <mattia.bertozzi@live.it>
2022-01-19 23:30:11 +0100

    Primo Commit

---
+++ Grani.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public static class Grani
+{
+    public static ulong Quadrato(int n)
+    {
+        if (n == 0)
+            throw new ArgumentOutOfRangeException("Il valore deve essere maggiore di 0");
+        if (n < 0)
+            throw new ArgumentOutOfRangeException("Il valore deve essere maggiore di 0");
+        if (n > 64)
+            throw new ArgumentOutOfRangeException("Il valore deve essere maggiore di 0");
+
+        //array contenente le 64 celle della scacchiera
+        ulong[] arrayScacchiera = ComponiQuadrato();
+
+        return arrayScacchiera[n-1];
+    }
+
+    public static ulong Total()
+    {
+        ulong Qta=0;
+        ulong[] arrayScacchiera = ComponiQuadrato();
+
+        for(int i=0;i<arrayScacchiera.Length;i++)
+        {
+            Qta+=arrayScacchiera[i];
+        }
+
+        return Qta;
+    }
+
+    public static ulong[] ComponiQuadrato()
+    {
+        ulong[] arrayScacchiera = new ulong[64];
+
+        arrayScacchiera[0]=1;
+
+        for(int i=1;i<arrayScacchiera.Length;i++)
+        {
+            arrayScacchiera[i]=arrayScacchiera[i-1]*2;
+        }
+        return arrayScacchiera;
+    }
+}
